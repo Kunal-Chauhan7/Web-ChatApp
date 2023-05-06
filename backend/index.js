f90a796f3729fde10da28b89ada1e770944063e9
Kunal Chauhan <kunalchauhanlion@gmail.com>
2023-05-06 19:17:53 +0530

    made the backend of the app

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { default: axios } = require("axios");
+const axios  = require("axios");
 
 const app = express();
 app.use(express.json());
@@ -9,22 +9,16 @@ app.use(cors({ origin: true }));
 app.post("/authenticate", async (req, res) => {
     const { username } = req.body;
     try {
-        const r = await axios.put(
-            'https://api.chatengine.io/users/',
-            {
-                username: username,
-                secret: username,
-                first_name : username
-            },
-            {
-                headers:{
-                    "private-key": "e43efb58-1d84-42e8-a5ee-5a4467e122c7"
-                }
-            }
-        )
-        return r.status(r.status).json(r.data)
-    } catch (error) {
-        return r.status(r.response.status).json(r.response.data)
+        const r = await axios.post(
+        "https://api.chatengine.io/users/",
+        {username: username,
+        secret: username, 
+        first_name : username},
+        { headers: {"private-Key":"e43efb58-1d84-42e8-a5ee-5a4467e122c7"}}
+        );  
+        return res.status(r.status).json(r.data);
+    } catch (e) {
+        return res.status(e.response.status).json(e.response.data);
     }
 });
 

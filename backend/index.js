49b11f7d34f23a89bdcfc076e6a05dcadc6a4ce2
Kunal Chauhan <kunalchauhanlion@gmail.com>
2023-05-06 18:48:01 +0530

    connected node to chatengine

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { default: axios } = require("axios");
 
 const app = express();
 app.use(express.json());
@@ -7,7 +8,24 @@ app.use(cors({ origin: true }));
 
 app.post("/authenticate", async (req, res) => {
     const { username } = req.body;
-    return res.json({ username: username, secret: "sha256..." });
+    try {
+        const r = await axios.put(
+            'https://api.chatengine.io/users/',
+            {
+                username: username,
+                secret: username,
+                first_name : username
+            },
+            {
+                headers:{
+                    "private-key": "e43efb58-1d84-42e8-a5ee-5a4467e122c7"
+                }
+            }
+        )
+        return r.status(r.status).json(r.data)
+    } catch (error) {
+        return r.status(r.response.status).json(r.response.data)
+    }
 });
 
 app.listen(3001);
